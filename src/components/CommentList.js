a26f7a914a5ad2d61606106abc218217a5ebd2fb
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-02 20:26:58 +0530

    making CommentList and commentBox components and putting boilerplate code

---
+++ src/components/CommentList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CommentList extends Component {
+
+    render() {
+        return (
+            <div>
+                CommentList
+            </div>
+        );
+    }
+}
+
+export default CommentList;
