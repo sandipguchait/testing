d2dc945c7afe7bed43aabcb1cf49a8c858c63523
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-03 20:59:03 +0530

    adding a describe block to group text are and share similar code btween them without repeating

--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -4,6 +4,7 @@ import CommentBox from '../CommentBox';
 
 
 let wrapped;
+
 beforeEach(() => {
      wrapped = mount(<CommentBox/>);
 });
@@ -17,20 +18,25 @@ it('has a text area and a button', () => {
     expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a textarea that users can type in', () => {
-    wrapped.find('textarea').simulate('change', {
-        target: { value: 'new comment'}
+// TEXT area Grouping 
+describe('the text area', () => {
+     // ONLY WILL RUN BEFORE THESE 2 Block of IT and not outside
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            target: { value: 'new comment'}
+        });
+        wrapped.update();
     });
-    wrapped.update();
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+
+  
+    it('has a textarea that users can type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+    });
+    
+    it('has textarea emptied after form gets submitted', ()=> {
+        wrapped.find('form').simulate('submit');
+        wrapped.update();
+        expect(wrapped.find('textarea').prop('value')).toEqual('')
+    });
 });
 
-it('has textarea emptied after form gets submitted', ()=> {
-    wrapped.find('textarea').simulate('change', {
-        target: { value: 'new comment'}
-    });
-    wrapped.update();
-    wrapped.find('form').simulate('submit');
-    wrapped.update();
-    expect(wrapped.find('textarea').prop('value')).toEqual('')
-})
