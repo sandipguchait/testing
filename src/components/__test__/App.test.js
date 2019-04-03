4f762f80871293519fecd68ec9ee482ba62fac43
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-03 17:21:13 +0530

    using BeforeEach() to refactor the code

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -4,13 +4,17 @@ import App from '../App';
 import CommentBox from '../CommentBox';
 import CommentList from '../CommentList';
 
+let wrapped;
+//rbeforeEach() run before each line of code with expect()
+beforeEach(() => {
+      wrapped = shallow(<App/>);
+})
+
 it('shows a comment box', () => {
-     const wrapped = shallow(<App/>);
      //writing the expect function for comment box to .toEqual mather
      expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it('shows a comment List', () => {
-     const wrapped = shallow(<App/>);
      expect(wrapped.find(CommentList).length).toEqual(1)
 });
