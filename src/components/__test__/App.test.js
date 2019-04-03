a85ef9f99d33735b9b5afe6d01343cc2605969fd
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-03 17:10:24 +0530

    writing shallow test for commentList

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
 it('shows a comment box', () => {
      const wrapped = shallow(<App/>);
      //writing the expect function for comment box to .toEqual mather
      expect(wrapped.find(CommentBox).length).toEqual(1);
-})
+});
+
+it('shows a comment List', () => {
+     const wrapped = shallow(<App/>);
+     expect(wrapped.find(CommentList).length).toEqual(1)
+});
