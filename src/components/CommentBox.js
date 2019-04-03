c330f176674c4f676c83ff076c5b31c56f18b62a
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-03 17:38:03 +0530

    adding a controolled form on the comment box

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 
 class CommentBox extends Component {
 
+    state = {
+        comment: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({ comment: event.target.value })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({ comment: '' })
+    }
+
     render() {
         return (
-            <div>
-                CommentBox
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <h4> Add a Comment </h4>
+                <textarea value={this.state.comment} onChange={this.handleChange}/>
+                <div>
+                   <button type="submit">Submit Comment</button>
+                </div>
+            </form>
         );
     }
 }
