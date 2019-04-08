92ce3081cae56e4bd8346d1e572191ca02544004
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-08 16:45:41 +0530

    removing redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers/index';
-
+import Root from './Root';
 
 ReactDOM.render(
-    <Provider store={createStore(reducers, {})}>
+    <Root>
         <App />
-    </Provider>
-    , document.getElementById('root'));
+    </Root>
+, document.getElementById('root'));
