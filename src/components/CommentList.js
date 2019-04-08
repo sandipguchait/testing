601eaa124559399d339e4c765742fc7b0da63dde
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-08 18:11:44 +0530

    connecting the state and rendering Comments as li

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class CommentList extends Component {
+
+    renderComments(){
+        return this.props.comments.map(comment => {
+            return <li key={comment}>{comment}</li>
+        });
+    }
 
     render() {
         return (
             <div>
-                CommentList
+                <ul>
+                    {this.renderComments()}
+                </ul>
             </div>
         );
     }
 }
 
-export default CommentList;
+const mapStateToprops = ( state ) => {
+    return {
+        comments: state.comments
+    }
+}
+
+export default connect(mapStateToprops)(CommentList);
